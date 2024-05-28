e800001ce14072b4ae4b7bbdf0059b9a86e0f3f1
safarov-en <safarov.elkhan110@gmail.com>
2024-05-28 18:02:57 +0500

    Adding routes

--- src/router.js
+++ src/router.js
@@ -1,12 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "./views/Login.vue";
+import Forget from "./views/Forget.vue";
+import Dashboard from "./views/Dashboard.vue";
+import Mail from "./views/Mail.vue";
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/login',
-            component: Login
+            component: Login,
+            alias: '/'
+        },
+        {
+            path: '/forget',
+            component: Forget
+        },
+        {
+            path: '/dashboard',
+            component: Dashboard
+        },
+        {
+            path: '/mail',
+            component: Mail
         }
     ]
 })
